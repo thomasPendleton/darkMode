9db0c78e0ad503ff8eb95ffe8d92e711d694d891
Thomas Pendleton <thomas6576@yahoo.com>
2022-07-08 12:32:56 -0700

    finished project

--- src/App.js
+++ src/App.js
@@ -2,10 +2,46 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const getStorageTheme = () => {
+  let theme = 'light-theme'
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme())
+
+  useEffect(() => {
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  const handleClick = () => {
+    if (theme === 'light-theme') {
+      return setTheme('dark-theme')
+    } else {
+      return setTheme('light-theme')
+    }
+  }
+
+  return (
+    <main>
+      <nav className="nav-center">
+        <h1>overreacted</h1>
+        <button className="btn" onClick={handleClick}>
+          toggle
+        </button>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />
+        })}
+      </section>
+      dark mode starter
+    </main>
+  )
 }
 
 export default App
-
-
--- src/Article.js
+++ src/Article.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+
+const Article = ({ date, snippet, title, length }) => {
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>{moment(date).format('MMMM Do, YYYY ddd')}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  )
 }
 
 export default Article
-
